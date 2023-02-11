1a4dbeca687c5482e81032a3e7447caf8fdfa6f7
AbhijitSridhar <abhijt.sridhar@gmail.com>
2023-02-11 10:50:01 -0500

    Added file access and updated db method

--- AuthenticatorApp/WebServer.cs
+++ AuthenticatorApp/WebServer.cs
@@ -8,46 +8,144 @@ using Windows.Networking.Sockets;
 using Windows.Storage.Streams;
 using System.IO;
 using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Storage;
 
 namespace AuthenticatorApp
 {
     internal class WebServer
     {
-        sealed partial class App : Application
+        public class HTTPServer : IDisposable
         {
-            int port = 8000;
 
-            /// <summary>
-            /// Initializes the singleton application object.  This is the first line of authored code
-            /// executed, and as such is the logical equivalent of main() or WinMain().
-            /// </summary>
-            public App()
+            private static HTTPServer instance;
+
+            public static HTTPServer Instance
             {
-                StartServer();
+                get
+                {
+                    if (instance == null)
+                    {
+                        instance = new HTTPServer(8000);
+                    }
+                    return instance;
+                }
             }
 
-            private void StartServer()
-            {
-                StreamSocketListener listener = new StreamSocketListener();
-                listener.BindServiceNameAsync(port.ToString());
-                Debug.WriteLine("Bound to port: " + port.ToString());
-                listener.ConnectionReceived += async (s, e) =>
-                {
-                    Debug.WriteLine("Got connection");
-                    using (IInputStream input = e.Socket.InputStream)
-                    {
-                        var buffer = new Windows.Storage.Streams.Buffer(2);
-                        await input.ReadAsync(buffer, buffer.Capacity, InputStreamOptions.Partial);
-                    }
 
-                    using (IOutputStream output = e.Socket.OutputStream)
+            private const uint BufferSize = 8192;
+
+            private readonly StreamSocketListener listener;
+            private int port;
+
+            public HTTPServer(int port)
+            {
+                this.port = port;
+                this.listener = new StreamSocketListener();
+                this.listener.ConnectionReceived += (s, e) => ProcessRequestAsync(e.Socket);
+            }
+
+            public void StartServer()
+            {
+#pragma warning disable CS4014
+                this.listener.BindServiceNameAsync(port.ToString());
+#pragma warning restore CS4014
+            }
+
+            public void Dispose()
+            {
+                this.listener.Dispose();
+            }
+
+            private async void ProcessRequestAsync(StreamSocket socket)
+            {
+                // this works for text only
+                StringBuilder request = new StringBuilder();
+                using (IInputStream input = socket.InputStream)
+                {
+                    byte[] data = new byte[BufferSize];
+                    IBuffer buffer = data.AsBuffer();
+                    uint dataRead = BufferSize;
+                    while (dataRead == BufferSize)
                     {
-                        using (Stream response = output.AsStreamForWrite())
+                        await input.ReadAsync(buffer, BufferSize, InputStreamOptions.Partial);
+                        request.Append(Encoding.UTF8.GetString(data, 0, data.Length));
+                        dataRead = buffer.Length;
+                    }
+                }
+
+                using (IOutputStream output = socket.OutputStream)
+                {
+                    HTTPRequest requestInfo = new HTTPRequest(request.ToString());
+
+                    if (requestInfo.Method == "GET")
+                    {
+                        wait WriteResponseAsync(requestInfo, output);
+                    }
+                    else
+                    {
+                        throw new InvalidDataException("HTTP method not supported: " + requestInfo.Method);
+                    }
+                }
+            }
+
+            private async Task WriteResponseAsync(HTTPRequest request, IOutputStream os)
+            {
+                using (Stream resp = os.AsStreamForWrite())
+                {
+                    bool exists = false;
+                    if (request.Path.StartsWith("/PUT_URL_HERE"))
+                    {
+                        exists = true;
+
+                        try
                         {
-                            response.Write(Encoding.ASCII.GetBytes("Hello, World!"), 0, 1);
+                            StorageFile fs = await StorageFile.GetFileFromPathAsync("FILEPATH");
+                            Stream readStream = await fs.OpenStreamForReadAsync();
+
+                            long length = readStream.Length;
+
+                            string header = String.Format("HTTP/1.1 200 OK\r\n" +
+                                "Connection: keep-alive\r\n" +
+                                "Content-Type: text/html\r\n" +
+                                "Content-Length: {1}\r\n" +
+                                "\r\n",,
+                                length);
+
+                            byte[] headerArray = Encoding.UTF8.GetBytes(header);
+                            await resp.WriteAsync(headerArray, 0, headerArray.Length);
+
+                            readStream.Position = start;
+
+                            var bytesToRead = length;
+
+                            byte[] buffer = new byte[BufferSize];
+                            int bytesRead = 0;
+                            while (bytesToRead > 0 && (bytesRead = await readStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                            {
+                                bytesToRead -= bytesRead;
+                                await resp.WriteAsync(buffer, 0, bytesRead);
+                            }
+
+                        }
+                        catch (FileNotFoundException)
+                        {
+                            exists = false;
                         }
                     }
-                };
+
+                    if (!exists)
+                    {
+                        byte[] headerArray = Encoding.UTF8.GetBytes(
+                                              "HTTP/1.1 404 Not Found\r\n" +
+                                              "Content-Length:0\r\n" +
+                                              "Connection: close\r\n\r\n");
+                        await resp.WriteAsync(headerArray, 0, headerArray.Length);
+                    }
+
+                    await resp.FlushAsync();
+                }
             }
         }
     }
