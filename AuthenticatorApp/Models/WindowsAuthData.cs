8c050527ef96cdbe462a0b248fc24a18bd2bc57c
Manigandan Krishnakumar <manigandan.ashvin@gmail.com>
2023-02-13 12:56:58 -0500

    Code Refactor and Project restructure

---
+++ AuthenticatorApp/Models/WindowsAuthData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticatorApp.Models
+{
+    internal class WindowsAuthData
+    {
+        public bool flag;
+        public string message;
+        public WindowsAuthData(bool _flag, string _message) {
+            this.flag = _flag;
+            this.message = _message;
+        }
+    }
+}
